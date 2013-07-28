08fe97da9fe0922c2397d0ace5978da9fd8dbd26
kevinbheda <kevinbheda@gmail.com>
2013-07-29 04:24:21 +0530

    added form methods

--- js/app.js
+++ js/app.js
@@ -3,6 +3,25 @@ app.views= {};
 app.routers={};
 app.models={};
 app.collections={};
+
+function validateForm(){
+    	var book ={
+    		'book_name': $("#addbook_book_name").val(),
+    		'author': $("#addbook_author_name").val(),
+    		'status': $(".addbook_status").val(),
+		};
+
+		if(!book.book_name || !book.author || status==4)
+			return false;
+		return book; // if all the conditions satisfied return book instead of true
+	} 
+
+function clearFormFields(){
+	$("#addbook_book_name").val("");
+	$("#addbook_author_name").val("");
+	$(".addbook_status").val("4");
+}	
+
 					
 $(document).on("ready", function () {
 	app.appRouter = new app.routers.AppRouter();
@@ -10,6 +29,4 @@ $(document).on("ready", function () {
 		Backbone.history.start({});   
 	});
 });
-
-
 
