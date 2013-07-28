c45e1db1ac8e789efdbae958516e153fcb9466ce
kevinbheda <kevinbheda@gmail.com>
2013-07-29 04:28:45 +0530

    non-event function

--- js/views/editbookview.js
+++ js/views/editbookview.js
@@ -1,6 +1,6 @@
 var app=app || {};
-app.views.EditBookView=Backbone.View.extend({
-
+app.views.EditBookView=Backbone.View.extend({ 
+	//almost smiliar to bookitemform view	
 	initialize: function(){
 		console.log("EditBookView initialized");
 	},
@@ -11,40 +11,81 @@ app.views.EditBookView=Backbone.View.extend({
 		this.$(".heading").html("Edit Book");
 		this.$(".addbook_status").val(this.model.get("status"));
 		this.delegateEvents({
-			'click .btn': 'save'
+			'click #addbook_button': 'save',
+			'click #deletebook_button': 'delete'
 		});
 		return this;
 	},
 
 	save: function(e){
 		e.preventDefault();
-		var book ={
-			'bookname': this.$("#addbook_book_name").val(),
-			'author': this.$("#addbook_author_name").val(),
-			'status': this.$(".addbook_status").val()
+		e.stopPropagation(); // prevent default form submission action
+		var book =validateForm();
+
+		if(!book)
+			alert("Please fill in the details"); 
+		else {   
+			var attr=this.model.changedAttributes(book);   //get attributes which user has edited 
+
+			if(!attr){   //if user has not edited anyfields
+				alert("Update something to save");	
+			}
+			else {
+				this.model.set(book);
+				var url=this.model.url()+this.model.get("id");
+				this.model.save(attr,{"url": url,
+					patch: true,  //send patch request to update only changed attributes
+					success: function(model,response){
+						if(response.result)
+							alert("Book updated successfully");
+
+						app.appRouter.navigate("#",{trigger: true, replace: true}); 
+					},
+					error: function(model,response){
+						alert("failed to save");
+						app.appRouter.navigate("#",{trigger: true, replace: true}); //triger router event and not stored in history
+					}
+				}); 
+			}
 		}
+	},
 
-		if((!book.bookname) || (!book.author) || book.status=='Choose Read status'){
-			alert("Please fill in the details");
+	delete: function(){
+		var book=validateForm();
+
+		if(!book){
+			alert("Some fields not set");
 		}
-		else {
-			this.model.set(book);
-			this.model.save(this.model.attributes,{
+		else{
+			var attr=this.model.changedAttributes(book);
 
-				success: function(model,response){
-					console.log(response);
+			if(attr &&
+				confirm("You have made some changes,would you still like to delete it?")
+			)// asks for confirmation for delete after changes 
+				this.deletebook();
+			else if(!attr) // direct delete
+				this.deletebook();
+			
+		}
+	},
 
-					if(response.result)
-					alert("Book updated successfully");
+	//non-event func
+	deletebook: function(){
+		var url=this.model.url()+this.model.get("id");
+		this.model.destroy({ wait:true,"url":url,
+			success: function(model,response,options){
 
-					app.appRouter.navigate("#",{trigger: true, replace: true});
-				},
-				
-				error: function(model,response){
-					alert("failed to save");
+				if(response.result){
+					alert("Book deleted successfully");
 					app.appRouter.navigate("#",{trigger: true, replace: true});
 				}
-			});
-		}
+				else 
+					alert("Failed to delete");
+			}, 
+			error: function(model,response,options){
+				alert("Failed to save");
+			}
+		});
+
 	}
 });
