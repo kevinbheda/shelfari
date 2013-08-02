7b197e8091c44fc1564fc569645d436745cf7bae
kevinbheda <kevinbheda@gmail.com>
2013-08-03 03:53:21 +0530

    refactored(jslint)

--- js/app.js
+++ js/app.js
@@ -1,32 +1,29 @@
-var app=app|| {};
-app.views= {};
-app.routers={};
-app.models={};
-app.collections={};
+/*global $,Backbone */
+var app = app || {};
+app.views = {};
+app.routers = {};
+app.models = {};
+app.collections = {};
 
-function validateForm(){
-    	var book ={
-    		'book_name': $("#addbook_book_name").val(),
-    		'author': $("#addbook_author_name").val(),
-    		'status': $(".addbook_status").val(),
-		};
+function validateForm() {
+    var book = {
+        'book_name': $("#addbook_book_name").val(),
+        'author': $("#addbook_author_name").val(),
+        'status': $(".addbook_status").val()
+    };
+    if (!book.book_name || !book.author || book.status === 4) {return false; }
+    return book; // if all the conditions satisfied return book instead of true 
+}
 
-		if(!book.book_name || !book.author || status==4)
-			return false;
-		return book; // if all the conditions satisfied return book instead of true
-	} 
+function clearFormFields() {
+    $("#addbook_book_name").val("");
+    $("#addbook_author_name").val("");
+    $(".addbook_status").val("4");
+}
 
-function clearFormFields(){
-	$("#addbook_book_name").val("");
-	$("#addbook_author_name").val("");
-	$(".addbook_status").val("4");
-}	
-
-					
 $(document).on("ready", function () {
-	app.appRouter = new app.routers.AppRouter();
-	$(function() {
-		Backbone.history.start({});   
-	});
+    app.appRouter = new app.routers.AppRouter();
+    $(function () {
+        Backbone.history.start();
+    });
 });
-
--- js/routers/approuter.js
+++ js/routers/approuter.js
@@ -1,46 +1,50 @@
-var app=app || {};
+/*global $,app,Backbone */
+var app = app || {};
 app.routers.AppRouter = Backbone.Router.extend({
 
-routes: {
-	"": "welcome",
-	"menu-item/addbook": "addbook",
-	"menu-item/searchbook": "searchbook",
-	"edit/:id": "editbook"
-},
+    routes: {
+        "": "welcome",
+        "menu-item/addbook": "addbook",
+        "menu-item/searchbook": "searchbook",
+        "edit/:id": "editbook"
+    },
 
-welcome: function(){
-	if(!app.randomBooksView)
-		app.randomBooksView=new app.views.BookListView({model: new app.collections.BookItemCollection()});
-	app.randomBooksView.model.reset();
-	app.randomBooksView.model.fetch();
-	$('#app').html(app.randomBooksView.render().el);
-},
+    welcome: function () {
+        if (!app.randomBooksView) {
+            app.randomBooksView = new app.views.BookListView({ model: new app.collections.BookItemCollection() });
+        }
+        app.randomBooksView.model.reset();
+        app.randomBooksView.model.fetch();
+        $('#app').html(app.randomBooksView.render().el);
+    },
 
-searchbook: function (){
-	if(!app.searchView)
-		app.searchView= new app.views.SearchBookView();
-	var myview=app.searchView.render().el;
-	$('#app').html(myview);
-},
+    searchbook: function () {
+        if (!app.searchView) {
+            app.searchView = new app.views.SearchBookView();
+        }
+        var myview = app.searchView.render().el;
+        $('#app').html(myview);
+    },
 
-addbook: function (){
-	if(!app.addBookView)
-		app.addBookView=new app.views.BookItemForm({model:new app.models.BookItem()});
-	var myview=app.addBookView.render().el;
-	$('#app').html(myview);
-},
+    addbook: function () {
+        if (!app.addBookView) {
+            app.addBookView = new app.views.BookItemForm({model : new app.models.BookItem()});
+        }
+        var myview = app.addBookView.render().el;
+        $('#app').html(myview);
+    },
 
-editbook: function(id){	
-	if (!isNaN(id)){
-		var book=new app.models.BookItem({"id":id});
-		var url=book.url()+book.get("id");
-		app.editBookView=new app.views.EditBookView({model:book});
-			book.fetch({"url": url,
-			success: function(model,response){
-				var myview=app.editBookView.render().el;
-				$('#app').html(myview);
-			}
-		});
-	}
-}
+    editbook: function (id) {
+        if (!isNaN(id) && id !== 0) {
+            var book = new app.models.BookItem({"id": id});
+            var url = book.url() + book.get("id");
+            app.editBookView = new app.views.EditBookView({model : book});
+            book.fetch({"url": url,
+                success: function (model, response, options) {
+                    var myview = app.editBookView.render().el;
+                    $('#app').html(myview);
+                }
+                });
+        }
+    }
 });
